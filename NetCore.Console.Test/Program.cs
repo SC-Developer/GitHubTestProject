7cc5422dabbde57a8ea838e8d989869f3cf8146f
Salman <Salman@e-itsolutions.co.uk>
2017-09-25 09:49:37 +0100

    Add project files.

---
+++ NetCore.Console.Test/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace NetCore.Console.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World SC!");
+            var name = System.Console.ReadLine();
+            System.Console.WriteLine("Hello " +name);
+            System.Console.ReadLine();
+
+        }
+    }
+}
